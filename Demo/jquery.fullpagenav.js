65c556a595971c174e55b1af9cc73a73001929fa
peachananr <peachananr@gmail.com>
2013-11-14 22:41:12 +0700

    Initial release

---
+++ Demo/jquery.fullpagenav.js
@@ -0,0 +1,150 @@
+/* ===========================================================
+ * jquery-fullpagenav.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Divide animated navigation into columns
+ * filling up the whole page with a simple JS call
+ *
+ * https://github.com/peachananr/fullpagenav
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    columns: 5,
+    selector: "> li",
+    hover_size: "30%",
+    animateDuration: 500,
+    animateFrom: "left",
+    clickable: true,
+    afterClicked: null
+	};
+	
+	$.fn.recalculate = function(settings, width) {
+	  var el = $(this),
+	      active = false,
+	      total = el.find(settings.selector).length,
+	      last_pos = 0,
+	      total_width = 0;
+	      
+	  if(el.find(".fpn_li.active").length > 0) {
+	  
+	    el.find(".fpn_li.active").css({
+        width: settings.hover_size
+      });  
+
+      
+      var small_width = (100 - parseFloat(settings.hover_size))/(settings.columns - 1)
+      
+      el.find(".fpn_li:not(.active)").css({
+        width: small_width + "%"
+      });
+	    
+	    
+	    el.find(settings.selector).each(function( index, value ) {
+	      if ($(this).prev(".fpn_li").length > 0) {
+	        if($(this).prev(".fpn_li").hasClass("active")) {
+	          var w = settings.hover_size
+	        } else {
+	          var w = small_width
+	        }
+          var left = total_width + parseFloat(w)
+          
+          $(this).finish().animate({
+            left: left + "%"
+          }, settings.animateDuration, function() {
+            $(this).css({
+              left: left + "%"
+            });
+          })
+          
+	        total_width = total_width + parseFloat(w)
+        }
+      });
+    } else {
+      el.find(settings.selector).each(function( index, value ) {
+        $(this).finish().animate({
+          width: width + "%",
+          left: (width * index) + "%"
+        }, settings.animateDuration);
+      });
+    }
+      
+	}
+	
+	function determineDirection($el, pos){
+      var w = $el.width(),
+          middle = $el.offset().left + w/2;        
+      return (pos.pageX > middle ? 0 : 1);
+  }
+
+  $.fn.fullpagenav = function(options){
+    var settings = $.extend({}, defaults, options),
+        el = $(this),
+        width = 100 / settings.columns;
+        
+    el.addClass("fullpagenav").find(settings.selector).addClass("fpn_li")
+    el.parent().addClass("fpn_body")
+    
+    
+    el.find(settings.selector).each(function( index, value ) {
+      var li = $(this);
+      
+      
+      li.css({
+        width: width + "%",
+        left: (width * index) + "%"
+      });
+      li.wrapInner("<div class='fpn_wrap'></div>")
+      
+      if (settings.clickable == true && li.data("link")) {
+        li.css({cursor: "pointer"}).click(function(e) {
+          li.find(".fpn_wrap > img").css({
+            margin: 0,
+            padding: 0,
+            left: 0,
+            maxHeight: "inherit"
+          }).animate({
+            width: "100%"
+            })
+          li.find(".fpn_wrap").css({position: "fixed", "z-index": 99}).finish().animate({
+            width: "100%", top: 0, left: 0
+          }, settings.animationDuration, function() {
+            e.preventDefault()
+            if (typeof settings.afterClicked == 'function') settings.afterClicked(li.data("link"));
+            window.location.href = li.data("link");
+          });
+          
+        });
+      }
+      
+      li.mouseenter(function(e) {
+        $(this).finish().addClass("active")
+        el.recalculate(settings, width);
+        if (settings.animateFrom == "auto") {
+          
+          if(determineDirection(li, e) == 1) {
+            $(this).find(".fpn_wrap").finish().css({ float: "left"}).animate({width: el.find(".fpn_li.active").width()}, settings.animateDuration)
+          } else {
+            $(this).find(".fpn_wrap").finish().css({ float: "right"}).animate({width: el.find(".fpn_li.active").width()}, settings.animateDuration)
+          }
+          
+          
+        } else {
+          $(this).find(".fpn_wrap").finish().css({ float: settings.animateFrom}).animate({width: el.find(".fpn_li.active").width()}, settings.animateDuration)
+        }
+        
+      }).mouseleave(function() {
+        
+        $(this).removeClass("active")
+        el.recalculate(settings, width);
+        el.find(".fpn_wrap").finish().css({width: "100%"})
+      });
+    });
+    
+  }
+}(window.jQuery);
+
--- README.md
+++ README.md
@@ -1,4 +1,88 @@
-fullpagenav
-===========
-
+#Full Page Navigation by Pete R.
 Create a fully responsive and dynamic full page navigation with one JS call
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [BucketListly](http://www.bucketlistly.com)
+
+[![Full Page Navigation](http://www.thepetedesign.com/images/fullpagenav_image.png "Full Page Navigation")](http://www.thepetedesign.com/demos/fullpagenav_demo.html)
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/fullpagenav_demo.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on both desktop and smartphones have been tested. I have not test this on IE.
+
+## Basic Usage
+Full Page Navigation is a simple script that let you create a full page navigation (navigation shown as columns) dynamically with one JS call. You can define how your navigation will be divided, how fast/slow the animation will be and many more features.
+
+To add this to your website, simply include the latest jQuery library together with `jquery.fullpagenav.js`, and `fullpagenav.css` into your document's `<head>` and create an HTML markup as follows:
+
+````html
+<body>
+...
+  <ul class="nav">
+    <li> ... </li>
+    <li> ... </li>
+    <li> ... </li>
+    ...
+  </ul>
+...
+</body>
+````
+
+And you can add any kind of HTML tags that you wish for it to display in each column inside the `<li>` tag. You can use different tags if you wish. You will need to assign a new selector though. More info on that below. Once this is done, call the script like this:
+ 
+````javascript
+$(".nav").fullpagenav({
+  columns: 5, // Number of columns you wish the script to divide your navigation into. This should be relative to the number of li tags you have. The column should be 3 for the example above. The default value is 5.
+  selector: "> li", // In case you don't want to use ul and li, you can change the selector here. This option accepts CSS selector. The Default value is "> li"
+  hover_size: "30%", // This option let you determine how big to expand the navigation when the cursor is hovering the area. The option accepts a percentage value. The default value is "30%"
+  animateDuration: 500, // You can define the animation duration here which will be used globally throughout the plugin. This option accepts milliseconds without the unit. The default value is 500.
+  animateFrom: "left", // Determine which direction the hovered navigation should expand to. Available options are "left", "right", and "auto". Auto will determine the direction from where you cursor enters the navigation area. The default value is "left".
+  clickable: true, // Toggle this to true if you want the area to be clickable. Make sure to add an extra data-link attribute to the li tag containing the URL you wish the navigation to take to user to once clicked. The default value is true.
+  afterClicked: null // A callback which will be fired after the user clicked the navigation
+});
+````
+
+## Making it Clickable
+Here's an example on how to make the navigation clickable. Make sure you have the HTML markup as follows:
+
+````html
+<body>
+...
+  <ul class="nav">
+    <li data-link="http://www.bucketlistly.com"> ... </li>
+    <li data-link="http://www.bucketlistly.com"> ... </li>
+    <li data-link="http://www.bucketlistly.com"> ... </li>
+    ...
+  </ul>
+...
+</body>
+```` 
+Note: Don't forget to change the data-link attribute to the URL you prefer. 
+
+Now, simply toggle the clickable option to true like this.
+
+````javascript
+$(".nav").fullpagenav({
+  clickable: true
+});
+```` 
+And the script will automatically make the whole area clickable, with a nice animation expanding the clicked area to full page and load the URL.
+
+## Callbacks
+You can use this callback to perform actions after you clicked the navigation. This is perfect for when you want to populate the page with an ajax instead of a traditional page load.
+
+### afterClicked(URL)
+This callback gets called after the user clicked the navigation.
+
+````javascript
+  $(".main").fullpagenav({
+    afterClicked: function(link) {
+      ...
+    }
+  });
+````
+
+And that's a wrap for all the features available for this plugin. If you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#design), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- Tutorial (Coming Soon)
